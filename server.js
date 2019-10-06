776544ac9e7a44bfe4043193daca3cf717f05e5f
Vahe Sasunts <sasuntsv@tcd.ie>
2019-10-06 14:57:41 +0100

    API called on server side

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+
 const express = require('express');
 const app = express();
 const Vue = require("vue");
+var $ = jQuery = require('jquery')(window);
 
 const port = "3001";
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+app.get('/city/:name',sendWeather)
 
 app.use(express.static('website'))
 
+function sendWeather(req, res){
+    let city = req.params.name    
+    console.log(city)
+    console.log("Calling open Weather API...")
+    $.getJSON("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=metric&appid=6fccb263105c078b7f49de484b9363d1", function(json){
+        res.json(json)
+    })
+}
