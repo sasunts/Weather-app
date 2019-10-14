6ac821eb381575405ad92c684413452acfa0b452
Vahe Sasunts <sasuntsv@tcd.ie>
2019-10-14 11:19:51 +0100

    Added comments

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
+//Importing relevant libraries
 var jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const { window } = new JSDOM();
 const { document } = (new JSDOM('')).window;
 global.document = document;
-
 const express = require('express');
 const app = express();
 const Vue = require("vue");
 var $ = jQuery = require('jquery')(window);
 
-const port = "3001";
+//Assigning a port value
+const port = "8080";
 
+//Listen on port 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+//Api for function sendWeather
 app.get('/city/:name',sendWeather)
 
+//Host the website located in the folder
 app.use(express.static('website'))
 
+//Function which calls the open weather API and returns the json data as a responce. 
 function sendWeather(req, res){
     let city = req.params.name    
     console.log(city)
